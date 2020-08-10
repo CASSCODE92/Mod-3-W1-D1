b859e95067239ccb2e17a28ea1762fc5ca86e66e
CASSCODE92 <cassie2012abioye@gmail.com>
2020-08-10 01:06:26 -0400

    type.java

---
+++ type.java
@@ -0,0 +1,36 @@
+// 4. Write a Java program that reads a number in inches, 
+// converts it to meters.
+// Note: One inch is 0.0254 meter. 
+// Example: Input a value for inch: 1000 Expected Output: 1000.0 inch is 25.4 meters
+
+
+
+
+Import java.util.Scanner;
+
+ public class type { 
+
+   public static void main(String[] args) { 
+
+        Scanner type = new Scanner(System.in);
+        System.out.print("Enter number of inches."); 
+
+        int inch = type.nextInt();
+
+        type.close();
+        
+        int meter = inch * 0.0254;
+         
+        System.out.println(inch + " in." + "=" +  meter + " m");
+   }
+ }
+         
+
+        
+
+        
+
+
+
+        
+     
