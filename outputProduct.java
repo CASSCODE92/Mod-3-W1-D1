570a8f2e7efebf00b7b5d2f5377c328112e7d465
CASSCODE92 <cassie2012abioye@gmail.com>
2020-08-10 00:43:11 -0400

    gitcommit

---
+++ outputProduct.java
@@ -0,0 +1,23 @@
+// // 3. Write a Java program that takes two numbers as input
+//  and display the product of two numbers.
+// https://data-flair.training/blogs/read-java-console-input/ 
+// (Links to an external site.)
+// Angel prefers to use the scanner class in Java.
+
+// Example:
+// Input first number: 25
+// Input second number: 5
+// Expected Output: 25 x 5 = 125
+// testcommit
+
+public class outputProduct {
+    public static void main(String[] args){
+        int firstNumber = 3;
+        int secondNumber = 2;
+        int expectedOutput = firstNumber * secondNumber;
+        System.out.println(expectedOutput);
+
+    }
+}
+
+
